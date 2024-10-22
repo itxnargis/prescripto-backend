a743e729c68d9a4aeaa459b8de16369dca08f7f2
itxnargis <itxnargiskhatun@gmail.com>
2024-10-22 13:06:38 +0530

    Fix errors

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -71,12 +71,12 @@ const loginAdmin = async (req, res) => {
       }
 }
 
-const allDoctors = async(req, res) => {
+const allDoctors = async (req, res) => {
       try {
 
             const doctors = await doctorModel.find({}).select('-password')
-            res.json({success: true, doctors})
-            
+            res.json({ success: true, doctors })
+
       } catch (error) {
             console.log(error)
             res.json({ success: false, message: error.message })
